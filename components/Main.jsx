9937f2cf17488ddf6055b3cbbdfa3909016580d4
Sarah BERRI <110137427+SarahBerri@users.noreply.github.com>
2023-05-02 11:57:36 +0200

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -28,8 +28,8 @@ function Main() {
     return (
         <div id="home" className='w-full lg:h-[120vh] md:h-screen text-center'>
             <div className='max-w-[1240px] h-full w-full mx-auto p-2 flex items-center justify-center'>
-                <div className="pt-[50px]">
-                    <h1 className='py-4 text-[33px]'>
+                <div>
+                    <h1 className='py-[50px] text-[33px] '>
                         Salut&#44; je suis <span className='text-[#000]'> Sarah&#44;</span>
                     </h1>
 
