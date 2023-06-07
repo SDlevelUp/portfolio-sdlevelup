8554c331904ff7fcf64904a2360e866332fc5367
Sarah Berri <sarahberri@outlook.fr>
2023-06-07 21:07:16 +0200

    config minify css file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
 	reactStrictMode: true,
-	
+
+	webpack(config) {
+		config.optimization.minimizer.push(new CssMinimizerPlugin());
+		return config;
+	},
 };
